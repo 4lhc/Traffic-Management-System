289618401221f43057db23be77a39e8ea08e3d14
4lhc <444lhc@gmail.com>
2019-05-02 13:34:05 +0530

    Stream made grayscale

--- TMS/camera.py
+++ TMS/camera.py
@@ -3,13 +3,15 @@ import cv2
 class VideoCamera:
 
     def __init__(self):
-        # self.video = cv2.VideoCapture(0)
-        self.video = cv2.VideoCapture('../test/vid/h264-night.mp4')
+        self.video = cv2.VideoCapture(1)
+        # self.video = cv2.VideoCapture('../test/vid/h264-night.mp4')
+        self.video.set(cv2.cv2.CAP_PROP_FPS, 15)
 
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
-        ret, frame = self.video.read()
-        ret, jpg = cv2.imencode('.jpg', frame)
+        ret, frame = self.video.read(cv2.IMREAD_GRAYSCALE)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        ret, jpg = cv2.imencode('.jpg', gray)
         return jpg.tobytes()
